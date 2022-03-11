72f92fbca573989e5a176480c03f36e06b5086e2
SayJaviCano <jc@saysawa.com>
2022-03-11 11:20:51 +0100

    Pruebas de dos casos

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,10 +1,20 @@
 describe("Search elements", () => {
-  it("Search for elements with multiple results", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("Search for elements with multiple results", () => {
     cy.fixture("index").then((index) => {
       cy.get(index.searchBox).type("dress");
       cy.get(index.searchButton).click();
       cy.get(".lighter").should("contain", "dress");
+    });
+  });
+  it("Search for elementes with no results", () => {
+    cy.fixture("index").then((index) => {
+      cy.get(index.searchBox).type("camisa");
+      cy.get(index.searchButton).click();
+      cy.get(".alert").should("contain", "No results");
     });
   });
 });
